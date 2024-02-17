fb7dd0102297a67a2bb6c6ff046d83f48d6001b2
vishal patsariya <vishalpatsariya99@gmail.com>
2024-02-17 16:35:40 +0530

    Prisma ORM
    Used prisma

---
+++ hello_prisma/script.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+async function query() {
+    try {
+        const Newuser=await prisma.user.create({
+            data: {
+                email: "vishalpatsariya89@gmail.com",
+                name: "vishal"
+            }
+        });
+
+        console.log(Newuser);
+    } catch (error) {
+        console.error("Error creating user:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+
+async function allUsers(){
+    const allUsers=await prisma.user.findMany();
+    console.log(allUsers);
+}
+
+//query();
+allUsers();
